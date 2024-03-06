d260903552a702ff6687e1cb2180bebe6abf2874
Nomathwords <hunterdevoe3@gmail.com>
2024-03-06 09:52:05 -0600

    Clarified readme, added sum functionality, began work on poll command

--- bot.py
+++ bot.py
@@ -1,26 +1,35 @@
-# This example requires the 'message_content' intent.
-
 import discord
+from typing import Optional
+from discord import app_commands
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 
-client = discord.Client(intents=intents)
 
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+# Sync the tree commands
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+    await tree.sync()
+    print("Ready!")
+
+# Send messages based on chat keywords
 @client.event
 async def on_message(message):
 
-    message.content = message.content.lower() + (" ")
+    message.content = message.content.lower()
 
+    # Ignore all messages from Push Bot
     if message.author == client.user:
         return
 
-    if message.content.startswith('hello'):
+    if message.content.startswith('hello') and not message.content.startswith('hello '):
         await message.channel.send('Hello!')
 
     if message.content.startswith('hi'):
@@ -47,6 +56,49 @@ async def on_message(message):
     if message.content.startswith('uwu'):
         await message.channel.send("ᵘʷᵘ oh frick ᵘʷᵘ ᵘʷᵘ \n ᵘʷᵘ ᵘʷᵘ ᵘʷᵘ \n ᵘʷᵘ ᵘʷᵘ ᵘʷᵘ frick sorry guys \n ᵘʷᵘ ᵘʷᵘ ᵘʷᵘ ᵘʷᵘ \n ᵘʷᵘ ᵘʷᵘ sorry im dropping \n ᵘʷᵘ my uwus all over the \n ᵘʷᵘ place ᵘʷᵘ ᵘʷᵘ ᵘʷᵘ sorry")
 
+    if message.content.startswith('howdy'):
+        await message.channel.send("Howdy howdy howdy")
+
+    if message.content.startswith("hello bro"):
+        await message.channel.send("I'm a freak, just lmk...")
+
+# Test command to make Push Bot say hello
+@tree.command(
+    name = "testcommand",
+    description = "I'm testing a slash command! This will make the bot say hello!",
+)
+async def testcommand(interaction):
+    await interaction.response.send_message("Hello!")
+
+# Command to make Push Bot add numbers
+@tree.command()
+@app_commands.describe(
+    first_value='The first value you want to add something to',
+    second_value='The value you want to add to the first value',
+    third_value = 'The value you want to add to the second value'
+)
+async def add(interaction: discord.Interaction, first_value: int, second_value: int, third_value: Optional[int]):
+    """Adds two numbers together."""
+    third_value = third_value or 0
+    await interaction.response.send_message(f'{first_value} + {second_value} + {third_value} = {first_value + second_value + third_value}')
+
+# Command to create a poll in chat
+@tree.command()
+@app_commands.describe(
+    question = 'The question of the poll',
+    one = "The first value in the poll",
+    two = "The second value in the poll",
+    three = "The third value in the poll",
+    four = "The fourth value in the poll"
+)
+async def poll(interaction: discord.Interaction, question: str, one: str, two: str, three: Optional[str], four: Optional[str]):
     
+    # Get optional parmeters, if any
+    three = three or None
+    four = four or None
+
+    # Build array of question and poll values
+    my_poll = []
+        
 
 client.run('')
--- readme.md
+++ readme.md
@@ -4,13 +4,15 @@ How to run:
 
 1. Clone the repo into your IDE or command line
 2. Install Python v3.8+
-3. Run the `command py -3 -m pip install -U discord.py` to install the discord.py dependencies
-4. Run `py -3 example_bot.py` to start the code and turn the bot on
+3a. If on Windows, run the `command py -3 -m pip install -U discord.py` to install the discord.py dependencies
+3b. If on another device, run the command `python3 -m pip install -U discord.py`
+4a. If on windows, run `py -3 bot.py` to start the code and turn the bot on
+4b. If on another device, run `python3 bot.py`
 
 Small ideas:
 1. Polls
 2. Small games (Dice, Coin Flip)
 
-Large ideas: 
+Large ideas:
 1. Fetch the Fortnite item shop every night
 2. Fetch the new Fortnite jam tracks every Tuesday
